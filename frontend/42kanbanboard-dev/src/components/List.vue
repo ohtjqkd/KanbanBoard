6601f2523fa732f2bab439e1e12e756cc9b59621
ohtjqkd <ohtjqkd@gmail.com>
2022-05-19 12:04:44 +0900

    Error make project

---
+++ frontend/42kanbanboard-dev/src/components/List.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="list">
+        <h3 class="list-title">{{ card.title }}</h3>
+        <ul class="list-items">
+          <Ticket v-for="item in card.tickets" :key="item.content" :content="item.content"/>
+        </ul>
+        <button class="add-card-btn btn" v-on:click="addCard(card.title)">Add a card</button>
+    </div>
+</template>
+
+<script>
+import Ticket from '@/components/Ticket'
+import Modal from '@/components/ListModal'
+
+export default {
+  components: { Ticket, Modal },
+  props: ['card'],
+  methods: {
+    addCard: function (title) {
+      alert(title)
+      console.log('hello console')
+    }
+  },
+  data () {
+    return {
+      showModal: false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
